203da43c48996b21933ff44a5822f90bd19cdf08
Teofil-Ioan Vartolomei <36919163+TeoVti@users.noreply.github.com>
2020-06-05 16:44:55 +0200

    Update youtube.js

--- src/configs/youtube.js
+++ src/configs/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCFwpTCqXDzUp13iRZD-Z6T1JPw_C42iAQ";
+const KEY = "YOUR_KEY";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
